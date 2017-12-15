4f190962cebcba72dbae79870e660964631bf359
Max Muth <kontakt@maxi-muth.de>
2017-12-15 19:14:25 +0100

    Fix indentation

--- tests/test_spread.py
+++ tests/test_spread.py
@@ -31,7 +31,7 @@ def test_calculate_spread():
     exchange_two.last_ask_price = 14_600.0
     exchange_two.last_bid_price = 14_500.0
     spread = Spread(exchange_one, exchange_two)
-    assert spread.spread == 100
+    assert spread.spread == 100.0
 
     exchange_one = Bitstamp(CurrencyPair.BTC_EUR)
     exchange_one.last_ask_price = 14_600.0
@@ -44,7 +44,7 @@ def test_calculate_spread():
 
 
 def test_different_currencies_exception():
-    with pytest.raises(AttributeError   ):
+    with pytest.raises(AttributeError):
         exchange_one = Bitstamp(CurrencyPair.BTC_EUR)
         exchange_one.last_ask_price = 14_500.0
         exchange_one.last_bid_price = 14_500.0
