cf06ee222a1ab9803993350490abe767826233bd
Martin Wauligmann <martin@wauligmann.com>
2017-12-16 01:05:25 +0100

    account balance and buy operation skeletons for bitstamp

--- bitcoin_arbitrage/exchange/bitstamp.py
+++ bitcoin_arbitrage/exchange/bitstamp.py
@@ -22,3 +22,27 @@ class Bitstamp(Exchange):
         self.last_ask_price = float(json.get('ask'))
         self.last_bid_price = float(json.get('bid'))
 
+    def get_account_balance(self) -> float:
+        url = f"{self.base_url}/balance/"
+        response = requests.post(url, data={
+            'key': '',
+            'signature': '',
+            'nonce': ''
+        })
+        json = response.json()
+        eur_balance = float(json.get('eur_balance'))
+        return eur_balance
+
+    def buy(self) -> str:
+        url = f"{self.base_url}/buy/{self.currency_pair}/"
+        response = requests.post(url, data={
+            'key': '',
+            'signature': '',
+            'nonce': '',
+            'amount': '',
+            'price': '',
+            'limit_price': ''
+        })
+        json = response.json()
+        order_id = json.get('id')
+        return order_id
