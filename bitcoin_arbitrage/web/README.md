374657021ff52efe2c33ffc8111829073aea1196
Max Muth <kontakt@maxi-muth.de>
2017-12-18 13:15:49 +0100

    Add webui

---
+++ bitcoin_arbitrage/web/README.md
@@ -0,0 +1,4 @@
+We use Gentella Admin for the templates.
+
+Check out the GitHub repo for all the available widgets and components.
+https://github.com/afourmy/flask-gentelella
---
+++ bitcoin_arbitrage/web/app.py
@@ -0,0 +1,57 @@
+import csv
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from logging import Formatter, FileHandler
+import logging
+import os
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+
+# Tear down SQLAlchemy
+@app.teardown_request
+def shutdown_session(exception=None):
+    db.session.remove()
+
+
+@app.route('/')
+def index():
+    with open('spread_history.csv') as f:
+        spreads = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+        last_spread = spreads[len(spreads) - 1]
+    return render_template('index.html', spreads=spreads, last_spread=last_spread)
+
+
+# Error
+@app.errorhandler(403)
+def not_found_error(error):
+    return render_template('page_403.html'), 403
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('page_404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('page_500.html'), 500
+
+
+# Logs
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    format = '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    file_handler.setFormatter(Formatter(format))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+if __name__ == '__main__':
+    # run on port 5000 by default
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
