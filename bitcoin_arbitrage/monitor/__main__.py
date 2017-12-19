ddbef1c6a8b9fd89c964822b60869912bb5c8a42
Max Muth <kontakt@maxi-muth.de>
2017-12-19 23:03:47 +0100

    Remove monitor.main

--- bitcoin_arbitrage/monitor/__main__.py
+++
@@ -1,9 +0,0 @@
-# import os
-#
-# if not os.path.isfile('bitcoin_arbitrage/settings.py'):
-#     raise ValueError('Settings file missing. Please run scripts/copy-config')
-#
-# from monitor import Monitor
-#
-# monitor = Monitor()
-# monitor.start()  # ToDo: This call should not be blocking
