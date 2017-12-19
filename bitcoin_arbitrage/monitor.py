5e406d383816caea70042083b73c1345d2ee2c62
Max Muth <kontakt@maxi-muth.de>
2017-12-19 01:08:27 +0100

    First try for embedding monitor in flask app

--- bitcoin_arbitrage/monitor.py
+++ bitcoin_arbitrage/monitor.py
@@ -13,28 +13,9 @@ logger = setup_logger('Monitor')
 
 class Monitor:
     def __init__(self) -> None:
-        self._update_task: asyncio.Task = None
-        self._update_task_loop = None
-        self._is_update_task_started = False
         self._last_spreads: List[Spread] = []
-        self._update_interval = settings.UPDATE_INTERVAL
 
-    def start(self) -> None:
-        if not self._is_update_task_started:
-            self._update_task = asyncio.Task(self._update())
-            self._update_task_loop = asyncio.get_event_loop()
-            self._update_task_loop.call_later(settings.UPDATE_INTERVAL, self.stop)
-
-            try:
-                self._update_task_loop.run_until_complete(self._update_task)
-            except asyncio.CancelledError:
-                pass
-
-    def stop(self) -> None:
-        if self._is_update_task_started:
-            self._update_task.cancel()
-
-    async def _update(self) -> None:
+    async def update(self) -> None:
         while True:
             logger.debug('Update...')
 
