62d214ad74701a6fc2682791ea0af30a8306d511
Max Muth <kontakt@maxi-muth.de>
2017-12-19 20:56:47 +0100

    Fix tests (test-script not yet working)

--- tests/test_notification.py
+++ tests/test_notification.py
@@ -1,11 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from update.notification.stdout import StdoutNotification
+from bitcoin_arbitrage.monitor.spread_detection import Spread # NOQA
+from bitcoin_arbitrage.monitor.update.notification.stdout import StdoutNotification
 
 
 def test_should_notify():
     notification = StdoutNotification()
     assert notification._should_notify(5) is True
     assert notification._should_notify(5) is False
-    notification._last_notification = datetime.now() - datetime.fromtimestamp(5)
+    notification._last_notification = datetime.now() - timedelta(seconds=5)
     assert notification._should_notify(5) is True
